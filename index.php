d8f327c3ea3da92a04e9f34b8b6e9387308f6a4f
1311059 <a.e.obayemi@rgu.ac.uk>
2016-03-08 10:50:56 +0000

    functions

--- index.php
+++ index.php
@@ -38,8 +38,8 @@ function wanted($name, $specs, $mnum, $snum){
     echo "Mugs". ' ' . $mnum . '<br>';
     echo "Suausage Rolls". ' ' . $snum . '<br>';
 
-    $award = 10 * ((pow(2, $specs*$mnum*$snum))/2);
-    echo "Award for capture". ' £' . number_format($award) . '<br>';
+    $award = 10 * (($specs*$mnum*$snum)^2/2);
+    echo "Award for capture". ' £' . $award . '<br>';
     //echo "Award for capture". ' £' . '<br>';
 }
 

abfdb7f45f01b2762f9a8899507fa8d5b7b98d66
1311059 <a.e.obayemi@rgu.ac.uk>
2016-03-14 10:31:39 +0000

    Adding a database connection file

---
+++ index.php
@@ -0,0 +1,27 @@
+<!Doctype html>
+<html>
+<head>
+
+</head>
+    <body>
+    <main>
+        <section id="loginModal">
+            <div>
+                <label for="username" />
+                <input type="text" name="username" id="username" value="" />
+            </div>
+            <div>
+                <label for="password" />
+                <input type="password" name="password" id="password" value="" />
+            </div>
+            <div>
+                <button>Submit</button>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+    </body>
+</html>

0b30aacaa119663eba074f0a2115837d144c6a53
1311059 <a.e.obayemi@rgu.ac.uk>
2016-03-14 10:27:39 +0000

    Adding a database connection file

--- connection.php
+++ connection.php
@@ -8,15 +8,15 @@
 
 // Defining constants for Db
 
-define('DBSERVER',"ap-cdbr-azure-east-c.cloudapp.net");
-define('DB_USERNAME', "b550786636ba9d");
-define('DB_PASSWORD', "70e4e13b");
-define('DB_DATABASE', "appstorage");
+define(DBSERVER,"ap-cdbr-azure-east-c.cloudapp.net");
+define(DB_USERNAME, "b550786636ba9d");
+define(DB_PASSWORD, "70e4e13b");
+define(DB_DATABASE, "appstorage");
 
 
 //creating a database connection
 //$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-echo DB_SERVER;
+echo DBSERVER;
 
 ?>

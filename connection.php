4627dd636a54dd5550bdb0b76e4ffb7f4daa0a2f
1311059 <a.e.obayemi@rgu.ac.uk>
2016-03-14 10:25:12 +0000

    Adding a database connection file

---
+++ connection.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1311059
+ * Date: 14/03/2016
+ * Time: 10:19
+ */
+
+// Defining constants for Db
+
+define(DBSERVER,"ap-cdbr-azure-east-c.cloudapp.net");
+define(DB_USERNAME, "b550786636ba9d");
+define(DB_PASSWORD, "70e4e13b");
+define(DB_DATABASE, "appstorage");
+
+
+//creating a database connection
+//$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+echo DB_SERVER;
+
+?>

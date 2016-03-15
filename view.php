09f9875daa0dc44aced4c158d724db718a0b4f4c
1311059 <a.e.obayemi@rgu.ac.uk>
2016-03-15 11:29:51 +0000

    Adding a database connection file

--- view.php
+++ view.php
@@ -1,21 +1,57 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1311059
- * Date: 15/03/2016
- * Time: 11:03
- */
+<?php session_start(); ?>
+    <!Doctype html>
+    <html>
+    <head>
+        <title>View All Movies</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
 
-include('connection.php');
-    if($_GET['done']){
+    </head>
+    <body>
+    <main>
+        <header>
+            <section id="home">
+                <div>
+                    <p>Welcome! <?php echo $_SESSION['user']; ?></p>
+                </div>
+                <nav>
+                    <ul id="nav">
+                        <li><a href="home.php">Home</a></li>
+                        <li><a href="view.php?done=a">Display All Movies</a></li>
+                        <li><a href="view.php?b">Display Movies Created by Marvel Studios</a></li>
+                        <li><a href="view.php?c">Display All Movies Created after 2010</a></li>
+                        <li><a href="view.php?d">Display all X-men Films</a></li>
+                    </ul>
+                </nav>
+            </section>
+
+        </header>
+        <section>
+            <?php
+            /**
+             * Created by PhpStorm.
+             * User: 1311059
+             * Date: 15/03/2016
+             * Time: 11:03
+             */
+
+            include('connection.php');
+            if($_GET['done']){
 
 
-            echo '<div>';
-            echo '<ul id="movieAll">';
-            echo '<li>MarvelMovieId</li><li>Year Released</li><li>Title</li><li>Production Studio</li><li>Notes</li>';
+                echo '<div>';
+                echo '<ul id="movieAll">';
+                echo '<li>MarvelMovieId</li><li>Year Released</li><li>Title</li><li>Production Studio</li><li>Notes</li>';
 
-            echo '</ul>';
-            echo '</div>';
+                echo '</ul>';
+                echo '</div>';
 
-    }
-?>
+            }
+            ?>
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+    </body>
+    </html>

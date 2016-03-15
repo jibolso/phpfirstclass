85fe14967300d277d449727022b3cc6119a50b69
1311059 <a.e.obayemi@rgu.ac.uk>
2016-03-15 11:09:31 +0000

    Adding a database connection file

---
+++ view.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1311059
+ * Date: 15/03/2016
+ * Time: 11:03
+ */
+
+include('connection.php');
+    if($_GET['a']){
+        $sql = "SELECT *";
+
+    }elseif($_GET['b']){
+
+    }elseif($_GET['c']){
+
+    }elseif($_GET['d']){
+
+    }else{
+
+    }
